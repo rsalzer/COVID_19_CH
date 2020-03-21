93c30104ef07e23d7626a8bf5590ab15dee926a8
Robert Salzer <robert.salzer@garzotto.com>
2020-03-21 21:21:58 +0100

    add visualisation of cantons

--- script.js
+++ script.js
@@ -80,6 +80,9 @@ function getCanton(i) {
 function processData() {
   console.log("Plotting data");
   processActualData();
+  for(var i=0; i<cantons.length; i++) {
+    barChartCases(cantons[i]);
+  }
 }
 
 function processActualData() {
@@ -122,7 +125,7 @@ function processActualData() {
     var td = document.createElement("td");
     td.appendChild(image);
     var a = document.createElement("a");
-    a.href = "#div_"+actual.canton;
+    a.href = "#div_"+actual.abbreviation_canton_and_fl;
     a.appendChild(document.createTextNode(" "+actual.abbreviation_canton_and_fl+":"));
     td.appendChild(a);
     tr.appendChild(td);
@@ -154,75 +157,13 @@ function processActualData() {
 }
 
 /*
-d3.csv('https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_kanton_total_csv/COVID19_Fallzahlen_Kanton_AG_total.csv', function (error, csvdata) {
-  data = csvdata;
-  for(var i=0; i<cantons.length; i++) {
-    barChartCases(cantons[i]);
-  }
-  var chLastDate = actualData[0].lastDate;
-  var h3 = document.createElement("h3");
-  var text = document.createTextNode("Aktueller Stand der positiv getesteten Fälle");
-  h3.appendChild(text);
-  document.getElementById("last").append(h3);
-  var sortedActual = Array.from(actualData).sort(function(a, b){return b.actual-a.actual});
-  var firstTable = document.createElement("table");
-  firstTable.id = "firstTable";
-  var secondTable = document.createElement("table");
-  secondTable.id = "secondTable";
-  for(var i=0; i<sortedActual.length; i++) {
-    var table;
-    if(i<sortedActual.length/2) table = firstTable;
-    else table = secondTable;
-    var actual = sortedActual[i];
-    var now = actual.actual;
-    var last = actual.last;
-    var diff = now-last;
-    var diffStr = diff>=0 ? "+"+diff : diff;
-    var lastDate = actual.lastDate;
-    var changeRatio = Math.round(diff/last * 100);
-    var changeRatioStr = changeRatio>=0 ? "+"+changeRatio+"%" : changeRatio+"%";
-    if(!last || last==0) changeRatioStr = "";
-    var alert = "";
-    if(lastDate!=chLastDate) alert = "(Daten vom "+lastDate+") ";
-    //console.log(lastDate+" : "+cantons[i]+": "+actual+" ("+diffStr+")");
-    var image = document.createElement("img");
-    image.height = 15;
-    image.src = "wappen/"+actual.canton+".png";
-    var tr = document.createElement("tr");
-    var td = document.createElement("td");
-    td.appendChild(image);
-    var text = document.createTextNode(now);
-    var a = document.createElement("a");
-    a.href = "#div_"+actual.canton;
-    td.appendChild(document.createTextNode(alert));
-    a.appendChild(document.createTextNode(" "+actual.canton+":"));
-    td.appendChild(a);
-    tr.appendChild(td);
-    td = document.createElement("td");
-    td.appendChild(text);
-    tr.appendChild(td);
-    td = document.createElement("td");
-    text = document.createTextNode(diffStr);
-    td.appendChild(text);
-    tr.appendChild(td);
-    td = document.createElement("td");
-    text = document.createTextNode(changeRatioStr);
-    td.appendChild(text);
-    tr.appendChild(td);
-    table.appendChild(tr);
-  }
-  document.getElementById("last").append(firstTable);
-  document.getElementById("last").append(secondTable);
-});
-
-/*
 d3.json('https://api.github.com/repos/openZH/covid_19/commits?path=COVID19_Cases_Cantons_CH_total.csv&page=1&per_page=1', function(error, data) {
   var lastUpdateDiv = document.getElementById('latestUpdate');
   lastUpdateDiv.innerHTML = "<i>Letztes Update der offiziellen Daten: "+data[0].commit.committer.date.substring(0,10)+" ("+data[0].commit.message+")</i>";
 });
 */
 
-
+/*
 d3.csv('https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland.csv', function(error, csvdata) {
   var div = document.getElementById("inofficial");
   var canvas = document.createElement("canvas");
@@ -291,9 +232,10 @@ d3.csv('https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/
     }
   });
 });
+*/
 
 function barChartCases(place) {
-  var filteredData = data.filter(function(d) { if(d.canton==place) return d});
+  var filteredData = data.filter(function(d) { if(d.abbreviation_canton_and_fl==place) return d});
   var div = document.createElement("div");
   div.id="div_"+place;
   var h2 = document.createElement("h2");
@@ -306,90 +248,88 @@ function barChartCases(place) {
   div.appendChild(h2);
   var canvas = document.createElement("canvas");
   //canvas.className  = "myClass";
-  canvas.id = place;
-  canvas.height=300;
-  canvas.width=300+filteredData.length*30;
-  div.appendChild(canvas);
-  document.getElementsByTagName('body')[0].appendChild(div);
-  var dateLabels = filteredData.map(function(d) {return d.date});
-  var testedPos = filteredData.map(function(d) {if(d.tested_pos=="NA") return 0; return d.tested_pos});
-  var confirmed = filteredData.map(function(d) {if(d.confirmed=="NA") return 0; return d.confirmed});
-  var actual = {};
-  actual.canton = place;
-  actual.actual = parseInt(testedPos[testedPos.length-1]);
-  if(testedPos.length<2) {
-    actual.last = 0
+  if(filteredData.length==0) {
+    div.appendChild(document.createTextNode("Keine Daten"));
+  }
+  else if(filteredData.length==1) {
+    div.appendChild(document.createTextNode("Ein Datensatz: "+filteredData[0].ncumul_conf+" Fälle am "+filteredData[0].date));
   }
   else {
-    actual.last = parseInt(testedPos[testedPos.length-2]);
+    canvas.id = place;
+    canvas.height=300;
+    canvas.width=300+filteredData.length*40;
+    div.appendChild(canvas);
   }
-  actual.lastDate = dateLabels[dateLabels.length-1];
-  actualData.push(actual);
-  var chart = new Chart(place, {
-    type: 'bar',
+  document.getElementsByTagName('body')[0].appendChild(div);
+  if(!filteredData || filteredData.length<2) return;
+  var dateLabels = filteredData.map(function(d) {
+    var dateSplit = d.date.split("-");
+    var day = parseInt(dateSplit[2]);
+    var month = parseInt(dateSplit[1])-1;
+    var year = parseInt(dateSplit[0]);
+    var date = new Date(year,month,day);
+    return date;
+  });
+  var cases = filteredData.map(function(d) {return d.ncumul_conf});
+  var chart = new Chart(canvas.id, {
+    type: 'line',
     options: {
       responsive: false,
       legend: {
         display: false
       },
-      plugins: {
-        labels: false
-      },
       title: {
         display: true,
-        text: 'Positive / Bestätigte Fälle '+names[place]
+        text: 'Bestätigte Fälle'
       },
-      tooltips: {
-						mode: "index",
-						intersect: true,
-			},
       scales: {
-        xAxes: [{
-                  stacked: true,
-                  id: "bar-x-axis1"
-                }],
-      yAxes: [{
-        stacked: false,
-        ticks: {
-          beginAtZero: true,
-          suggestedMax: 10,
+            xAxes: [{
+                type: 'time',
+                time: {
+                    tooltipFormat: 'D.MM.YYYY',
+                    unit: 'day',
+                    displayFormats: {
+                        day: 'D.MM'
+                    }
+                }
+            }]
         },
-      }]
-  }
-  /*
-      /*,
       plugins: {
-        labels: {
-          render: function (args) {
-               var index = args.index;
-               var value = args.value;
-               if(index==0) return "";
-               var lastValue = args.dataset.data[index-1];
-               var percentageChange = value/lastValue - 1;
-               var rounded = Math.round(percentageChange * 100);
-               var label = ""+rounded;
-               if(rounded >= 0) label = "+"+label+"%";
-               else label = "-"+label+"%";
-               return label;
-            }
+          datalabels: {
+  						color: 'black',
+  						font: {
+  							weight: 'bold'
+  						},
+  						formatter: function(value, context) {
+                var index = context.dataIndex;
+                if(index==0) return "";
+                var lastValue = context.dataset.data[index-1];
+                var percentageChange = value/lastValue - 1;
+                var rounded = Math.round(percentageChange * 100);
+                var label = ""+rounded;
+                if(rounded >= 0) label = "+"+label+"%";
+                else label = "-"+label+"%";
+
+                var change = value-lastValue;
+                var label = change>0 ? "+"+change : change;
+                return label;
+              }
+  					}
           }
-        }
-        */
     },
     data: {
       labels: dateLabels,
       datasets: [
         {
-          data: confirmed,
-          backgroundColor: 'rgba(200,20,20,120)',
-          borderWidth: 1,
-          label: "Bestätigt"
-        },
-        {
-          data: testedPos,
+          data: cases,
+          fill: false,
+          spanGaps: true,
+          borderColor: '#F15F36',
           backgroundColor: '#F15F36',
-          borderWidth: 1,
-          label: "Positiv getestet"
+          datalabels: {
+						align: 'end',
+						anchor: 'end'
+					}
         }
       ]
     }
