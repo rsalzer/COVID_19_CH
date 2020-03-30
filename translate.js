15e5b13305091d93c3e0d79c9d2d6a7872f872d0
Jeff R. Allen <jra@nella.org>
2020-03-30 22:54:56 +0200

    Proof of concept of translation.

---
+++ translate.js
@@ -0,0 +1,46 @@
+// Thanks to
+// http://www.ab-weblog.com/en/internationalization-how-to-localize-html5-projects/
+
+function loaded()
+{
+  var lang = getParameterValue("lang");
+  if (lang != "") String.locale = lang;
+
+  walk(document, function(n) {
+    if (n.nodeValue) {
+      if (n.nodeValue.toLocaleString() !== n.nodeValue) {
+        n.nodeValue = n.nodeValue.toLocaleString();
+      } else {
+        var t = n.nodeValue.trim()
+        if (t.toLocaleString() !== t) {
+          n.nodeValue = " " + t.toLocaleString() + " "
+        } else {
+          // Useful for finding translations to do.
+          console.log(`"${t}": ""`)
+        }
+      }
+    }
+  })
+  document.title = document.title.toLocaleString();
+}
+
+function getParameterValue(parameter)
+{
+  parameter = parameter.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + parameter + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.href);
+  if(results == null)
+    return "";
+  else
+    return results[1];
+}
+
+function walk(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walk(node, func);
+        node = node.nextSibling;
+    }
+};
