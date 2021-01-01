55d314da03590c5fccffc316b9e171bf816b2e01
Robert Salzer <robert.salzer@garzotto.com>
2021-01-01 13:47:05 +0100

    fix new years-bug ;-)

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function processActualData(mode, chosenDay) {
     td.appendChild(a);
     tr.appendChild(td);
     td = document.createElement("td");
-    td.appendChild(document.createTextNode(actual.date_ncumul_conf.replace("2020-", "")));
+    td.appendChild(document.createTextNode(actual.date_ncumul_conf.replace("2020-", "").replace("2021-", "")));
     tr.appendChild(td);
     td = document.createElement("td");
     var text = document.createTextNode(now);
@@ -402,7 +402,7 @@ function processActualHospitalisation(filter) {
     td.appendChild(a);
     tr.appendChild(td);
     td = document.createElement("td");
-    td.appendChild(document.createTextNode(getDeviceState()==2?actual.date_current_hosp.replace("2020-", ""):actual.date_current_hosp));
+    td.appendChild(document.createTextNode(getDeviceState()==2?actual.date_current_hosp.replace("2020-", "").replace("2021-", ""):actual.date_current_hosp));
     tr.appendChild(td);
     td = document.createElement("td");
     var text = document.createTextNode(now);
