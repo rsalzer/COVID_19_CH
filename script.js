9f37effbc54254493a93d6bdc84f8654822b014a
Robert Salzer <robert.salzer@garzotto.com>
2021-01-09 18:24:06 +0100

    deploy vergleich

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ Chart.defaults.global.defaultFontFamily = "IBM Plex Sans";
 document.getElementById("loaded").style.display = 'none';
 
 setLanguageNav();
-
+includeHTML();
 //console.log("START");
 getCanton(0);
 
@@ -1838,5 +1838,34 @@ function getWorldDeaths(chTotal) {
       tr.innerHTML = "<td>"+(start+i+1)+".</td><td>"+countryFlag+"</td><td>"+formattedCases+"</td>";
     }
     firstTable.appendChild(tr);
+  }
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("nav");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("url");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.insertAdjacentHTML("afterbegin", this.responseText);}
+          if (this.status == 404) {elmnt.insertAdjacentHTML("afterbegin", "Page not found.");}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("url");
+          document.getElementById("pagenav_zh").className = "here";
+          loaded();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
   }
 }
